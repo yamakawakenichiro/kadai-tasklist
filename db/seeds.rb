b8eae85c646abb1ede654060c1b2be0c136e0596
kenichiro <sutoratokenichirou@yahoo.jp>
2017-06-12 11:11:22 +0000

    bootstrap tasklist

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Task.create(status: 'status 1', content: 'test content 1')
+Task.create(status: 'status 2', content: 'test content 2')
+Task.create(status: 'status 3', content: 'test content 3')
